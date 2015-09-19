7b26c1592846c3a144d1dde818436d28a5470dd7
Riddhi Rathod <riddhirathod@gmail.com>
2015-09-18 20:04:51 -0400

    Added message to root page

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-		
+
+app.get('/', function(req, res){
+  var response = 'Main Page...Go to current_url/hello.html to view another page';
+  res.send(response);
+});		
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, ipaddress);
