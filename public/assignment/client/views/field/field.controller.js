945ad092d41daa9a19a5edc64692249808184cd9
Riddhi Rathod <riddhirathod@gmail.com>
2015-11-15 23:10:47 -0500

    modified field.view.html and field controller

--- public/assignment/client/views/field/field.controller.js
+++ public/assignment/client/views/field/field.controller.js
@@ -1,5 +1,5 @@
 /**
- * Created by riddhirathod on 11/10/15.
+ * Created by Riddhi Rathod on 11/10/15.
  */
 "use strict";
 (function () {
@@ -10,73 +10,81 @@
         var userId = $routeParams.userId;
         var formId = $routeParams.formId;
 
-        $scope.fieldsOption = [{ name: "Single Line Text Field", id: 0 },
+        $scope.fieldsOption = [
+            { name: "Single Line Text Field", id: 0 },
             { name: "Multi Line Text Field", id: 1},
             { name: "Date Field", id: 2},
             { name: "Dropdown Field", id: 3},
             { name: "CheckBoxes Field", id: 4 },
-            { name: "Radio Buttons Field", id: 5}];
+            { name: "Radio Buttons Field", id: 5}
+        ];
 
-        FieldService.getFieldsForForm(formId).then(function(fields){
-            $scope.fields = fields;
-        });
+        FieldService.getFieldsForForm(formId)
+            .then(function(fields){
+                $scope.fields = fields;
+            });
 
-        $scope.addField = function(modelType){
+        $scope.addField = addField;
+        $scope.removeField = removeField;
+        $scope.clone = clone;
+
+        function addField(modelType) {
             var textField = {"id": null, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"};
             var textAreaField = {"id": null, "label": "New Text Field", "type": "TEXTAREA", "placeholder": "New Field"};
             var dateField = {"id": null, "label": "New Date Field", "type": "DATE"};
-            var dropDownField = {"id": null, "label": "New Dropdown",
-                "type": "OPTIONS", "options":
-                    [ {"label": "Option 1", "value": "OPTION_1"},
-                        {"label": "Option 2", "value": "OPTION_2"},
-                        {"label": "Option 3", "value": "OPTION_3"} ]};
-            var checkBoxField = {"id": null, "label": "New Checkboxes",
-                "type": "CHECKBOXES", "options":
-                    [ {"label": "Option A", "value": "OPTION_A"},
-                        {"label": "Option B", "value": "OPTION_B"},
-                        {"label": "Option C", "value": "OPTION_C"} ]};
-            var radioBoxField = {"id": null, "label": "New Radio Buttons",
-                "type": "RADIOS", "options":
-                    [ {"label": "Option X", "value": "OPTION_X"},
-                        {"label": "Option Y", "value": "OPTION_Y"},
-                        {"label": "Option Z", "value": "OPTION_Z"} ]};
-            var field;
 
+            var dropDownField = {"id": null, "label": "New Dropdown", "type": "OPTIONS", "options": [
+                {"label": "Option 1", "value": "OPTION_1"},
+                {"label": "Option 2", "value": "OPTION_2"},
+                {"label": "Option 3", "value": "OPTION_3"}
+            ]};
+
+            var checkBoxField = {"id": null, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
+                {"label": "Option A", "value": "OPTION_A"},
+                {"label": "Option B", "value": "OPTION_B"},
+                {"label": "Option C", "value": "OPTION_C"}
+            ]};
+
+            var radioBoxField = {"id": null, "label": "New Radio Buttons", "type": "RADIOS", "options": [
+                {"label": "Option X", "value": "OPTION_X"},
+                {"label": "Option Y", "value": "OPTION_Y"},
+                {"label": "Option Z", "value": "OPTION_Z"}
+            ]};
+
+            var field;
             if(modelType === "Single Line Text Field"){
                 field = textField;
-            }
-            else if(modelType === "Multi Line Text Field"){
+            } else if(modelType === "Multi Line Text Field"){
                 field = textAreaField;
-            }
-            else if(modelType === "Date Field"){
+            } else if(modelType === "Date Field"){
                 field = dateField;
-            }
-            else if(modelType === "Dropdown Field"){
+            } else if(modelType === "Dropdown Field"){
                 field = dropDownField;
-            }
-            else if(modelType === "CheckBoxes Field"){
+            } else if(modelType === "CheckBoxes Field"){
                 field = checkBoxField;
-            }
-            else if(modelType === "Radio Buttons Field"){
+            } else if(modelType === "Radio Buttons Field"){
                 field = radioBoxField;
             }
 
             console.log(field);
-            FieldService.createFieldForForm(formId,field).then(function(fields){
-                $scope.fields =fields;
-            });
-        };
+            FieldService.createFieldForForm(formId, field)
+                .then(function(fields){
+                    $scope.fields = fields;
+                });
+        }
 
-        $scope.removeField = function(field){
-            FieldService.deleteFieldFromForm(formId,field.id).then(function(fields){
-                $scope.fields = fields;
-            });
-        };
+        function removeField(field){
+            FieldService.deleteFieldFromForm(formId, field.id)
+                .then(function(fields){
+                    $scope.fields = fields;
+                });
+        }
 
-        $scope.clone = function(field){
-            FieldService.cloneField(formId,field).then(function(fields){
-                $scope.fields = fields;
-            });
-        };
+        function clone(field){
+            FieldService.cloneField(formId,field)
+                .then(function(fields){
+                    $scope.fields = fields;
+                });
+        }
     }
 })();
