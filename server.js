9c200000007b7af3fc935c94332d1fddb776bbaa
Riddhi Rathod <riddhirathod@gmail.com>
2015-11-30 10:45:47 -0500

    fixed typo in server.js related to mongodb connection string

--- server.js
+++ server.js
@@ -29,16 +29,3 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ipaddress);
-
-
-var connectionString = 'mongodb://127.0.0.1:27017/cs5610fall2015exmpl1';
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-}
-
-var db = mongoose.connect(connectionString);
