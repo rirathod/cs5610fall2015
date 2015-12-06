fa7e6c211ed8c43382607abb74a1b9c35b9ed732
Riddhi Rathod <riddhirathod@gmail.com>
2015-12-05 20:03:51 -0500

    fixing code to update project status based on git mesaages

--- public/project/client/views/projectField/projectField.controller.js
+++ public/project/client/views/projectField/projectField.controller.js
@@ -11,25 +11,69 @@
         var userId = $routeParams.userId;
         var projectId = $routeParams.projectId;
 
+        function updateProjectStatus() {
+            if(($scope.project.commits.length > 1) && ($scope.project.status === "NOT STARTED")){
+                console.log("Updating project status from NOT STARTED to STARTED");
+                var newProject = {
+                    "status": "STARTED"
+                };
+                console.log(newProject);
+
+                ProjectService.updateProjectById(projectId, newProject)
+                    .then(function(updatedProject) {
+                        console.log(updatedProject);
+                        $scope.project = updatedProject;
+                    });
+            }
+        }
+
+        function syncGitCommits() {
+            if (!angular.isUndefined($scope.project.githubUsername) && $scope.project.githubUsername != ""
+                && !angular.isUndefined($scope.project.githubReponame) && $scope.project.githubReponame != "") {
+
+                // Syncing git commits for project
+                GithubService.syncCommits($scope.project.githubUsername, $scope.project.githubReponame)
+                    .then(function (commits) {
+                        console.log(commits);
+                        var projectCommits = [];
+                        for (var i = 0; i < commits.length; i++) {
+                            projectCommits.push({
+                                "committer": commits[i].commit.committer.name,
+                                "message": commits[i].commit.message,
+                                "timestamp": commits[i].commit.committer.date
+                            });
+                        }
+                        var newProject = {
+                            "commits": projectCommits
+                        };
+                        console.log(newProject);
+
+                        // Load project git commits
+                        ProjectService.updateProjectById(projectId, newProject)
+                            .then(function (updatedProject) {
+                                console.log(updatedProject);
+                                $scope.project = updatedProject;
+
+                                // Updating project status based on git commit messages
+                                console.log($scope.project.commits.length);
+                                console.log($scope.project.status);
+                                updateProjectStatus();
+                            });
+                    });
+            }
+        }
+
         ProjectService.findProjectById(projectId)
             .then(function(project){
-                //console.log(project);
-
-                //var project =  {
-                //    "_id": project._id,
-                //    "title": project.title,
-                //    "description": project.description,
-                //    "status": project.status,
-                //    "githubUsername": project.githubUsername,
-                //    "githubReponame": project.githubReponame
-                //};
-
                 $scope.project = project;
-                //console.log(project);
+                console.log("Before syncing git commits");
+                syncGitCommits();
+                console.log("After syncing git commits");
             });
 
+        // Project
         $scope.updateProject = updateProject;
-
+        // Project Sub-Tasks
         $scope.addProjectSubTask = addProjectSubTask;
         $scope.updateProjectSubTask = updateProjectSubTask;
         $scope.deleteProjectSubTask = deleteProjectSubTask;
@@ -47,41 +91,12 @@
                 .then(function(updatedProject) {
                     console.log(updatedProject);
                     $scope.project = updatedProject;
-
-                    console.log("Before syncing git commits");
-                    GithubService.syncCommits($scope.project.githubUsername, $scope.project.githubReponame)
-                        .then(function(commits) {
-                            console.log(commits);
-
-                            var projectCommits = [];
-                            for(var i=0; i<commits.length; i++) {
-                                projectCommits.push({
-                                    "committer": commits[i].commit.committer.name,
-                                    "message": commits[i].commit.message,
-                                    "timestamp": commits[i].commit.committer.date
-                                });
-                            }
-
-                            var newProject = {
-                                "commits": projectCommits
-                            };
-                            console.log(newProject);
-
-                            ProjectService.updateProjectById(projectId, newProject)
-                                .then(function(updatedProject) {
-                                    console.log(updatedProject);
-                                    $scope.project = updatedProject;
-                                });
-                        });
-                    console.log("After syncing git commits");
-
                 });
         }
 
         function addProjectSubTask() {
             if(!angular.isUndefined($scope.subTaskName) && $scope.subTaskName != ""){
                 console.log($scope.subTaskName);
-
                 var subTask = {
                     "name": $scope.subTaskName
                 };
@@ -92,10 +107,6 @@
                         $scope.subTaskName = "";
                     });
             }
-        }
-
-        function getProjectSubTasks() {
-
         }
 
         function updateProjectSubTask() {
